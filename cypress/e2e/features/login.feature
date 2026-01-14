31fe0bcfbef2b550541c188ccdcfbe462ed3c861
Roumaissa Chamma <roumaissa@visionspol.eu>
2026-01-14 12:18:51 +0100

    Add login test example

---
+++ cypress/e2e/features/login.feature
@@ -0,0 +1,5 @@
+Feature: Login demo
+
+  Scenario: User visits the login page
+    Given I open the login page
+    Then I fill the login form
---
+++ cypress/support/step_definitions/login.js
@@ -0,0 +1,12 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("I open the login page", () => {
+  cy.visit("https://dev.visionstrust.com/registration/login");
+});
+
+Then("I fill the login form", () => {
+  cy.get("#emailLogin").type("test@test.com");
+  cy.get("#passwordLogin").type("password123");
+  cy.get("#loginBtn").submit();
+});
+
